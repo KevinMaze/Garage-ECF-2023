b2fcecba5666b2419eb5fcfc657f9e1af6d48850
Kévin MAZE <kekeproject@hotmail.fr>
2023-07-23 17:57:19 +0200

    add navbar lateral

--- index.php
+++ index.php
@@ -1,15 +1,64 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/style.css">
-  <title>VP Garage</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="#">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/lib/nav.css">
+    <link rel="shortcut icon" href="assets/logo_VP.png" type="image/png">
+    <title>VP-Garage</title>
 </head>
 
-  <body>
+    <body>
+
+        <nav>
+            <aside id="sidebar">
+                <div class="sidebar_content sidebar_head">
+                    <h1>Sidebar</h1>
+                </div>
+
+                <div class="line"></div>
+
+                <div class="sidebar_content sidebar_body">
+                    <nav class="side_navlinks">
+                        <ul>
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="#">Nos services</a></li>
+                            <li><a href="#">Les occasions</a></li>
+                            <li><a href="#">Contact / Avis</a></li>
+                        </ul>
+                        <a href="/">Connection</a>
+                    </nav>
+                </div>
+
+                <div class="sidebar_content sidebar_foot">
+                    <p>
+                    &#169;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        &#160;VP Garage.
+                    </p>
+                </div>
+            </aside>
+
+            <div class="sidebar_toggler">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+
+        <header>
+            <img src="assets/logo_VP.png" alt="Logo VP Garage">
+        </header>
+
+
     
 
-  <script src="./js/script.js"></script>
-  </body>
+        <script src="./js/script.js"></script>
+        <script src="./js/lib/nav.js"></script>
+    </body>
 </html>
