fb09c7da23e3d08700d4cead73fb1a5daffcc208
Kévin MAZE <kekeproject@hotmail.fr>
2023-07-23 12:26:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>VP Garage</title>
+</head>
+
+  <body>
+    
+
+  <script src="./js/script.js"></script>
+  </body>
+</html>
