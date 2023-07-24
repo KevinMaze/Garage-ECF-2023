f5d13e5de282dd305c23c962dfe8dc9134a6132c
KevinMaze <kekeprojectphotmail.fr>
2023-07-24 15:51:04 +0200

    style, header

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/lib/nav.css">
+    <link rel="stylesheet" href="./css/lib/button.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed:wght@100;200;300;400;500;600;700;800;900&family=Marvel:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="assets/logo_VP.png" type="image/png">
     <title>VP-Garage</title>
 </head>
@@ -16,7 +20,7 @@
         <nav>
             <aside id="sidebar">
                 <div class="sidebar_content sidebar_head">
-                    <h1>Sidebar</h1>
+                    <h1 class="sidebar__title">VP Garage</h1>
                 </div>
 
                 <div class="line"></div>
@@ -28,10 +32,12 @@
                             <li><a href="#">Nos services</a></li>
                             <li><a href="#">Les occasions</a></li>
                             <li><a href="#">Contact / Avis</a></li>
+                            <a href="/" class="custom-button">Connection</a>
                         </ul>
-                        <a href="/">Connection</a>
                     </nav>
                 </div>
+
+                <div class="line"></div>
 
                 <div class="sidebar_content sidebar_foot">
                     <p>
@@ -44,6 +50,7 @@
                 </div>
             </aside>
 
+
             <div class="sidebar_toggler">
                 <span></span>
                 <span></span>
@@ -51,9 +58,26 @@
             </div>
         </nav>
 
-        <header>
-            <img src="assets/logo_VP.png" alt="Logo VP Garage">
+        <header class="header__background">
+            <div class="header__description flux">
+                <img src="assets/logo_VP.png" alt="Logo VP Garage">
+                <h1 class="header__title">Garage Automobile</h1>
+                <p class="header__para">Vincent Parrot, fort de ses 15 années d'expérience dans la réparation automobile, a ouvert 
+                    son propre garage à Toulouse en 2021.
+                    Depuis 2 ans, il propose une large gamme de services: réparation de la carrosserie et de la 
+                    mécanique des voitures ainsi que leur entretien régulier pour garantir leur performance et 
+                    leur sécurité.
+                    Vincent Parrot considère son atelier comme un véritable lieu de confiance pour ses clients et 
+                    leurs voitures doivent, selon lui, à tout prix être entre de bonnes mains.
+                </p>
+            </div>
         </header>
+
+        <div class="line-style flux"></div>
+
+        <section class="section__service flux">
+            <h2 class="section__service__title">service</h2>
+        </section>
 
 
     
